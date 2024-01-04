d60926a3784dd1c3a427c51dc537f009730665d1
rhysqi <rhysqi.akbar@gmail.com>
2024-01-04 15:10:34 +0700

    Adding some update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('keypress', (Event) =>
-{
+document.addEventListener('keypress', (Event) =>{
     let keycode = Event.key;
     var digit = Math.floor(Math.random(digit) * 3);
     let player = document.getElementById('out-player');
     let bot = document.getElementById('out-com');
+
     if (keycode == 1) {
         player.innerHTML = ' <img src="media/1.jpg" style="width: 80px; transform: rotate(-120deg); position: absolute; margin-top: 35px; margin-left: -40px" />';
         if (digit == 0){
@@ -17,6 +17,7 @@ document.addEventListener('keypress', (Event) =>
             document.getElementById("com-score").innerHTML = "Lose"; document.getElementById("Player-Score").innerHTML = "Win";
         }
     }
+
     else if( keycode == 2){
         player.innerHTML = ' <img src="media/2.jpg" style="width: 80px; transform: rotate(90deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
         if (digit == 0){
@@ -30,6 +31,7 @@ document.addEventListener('keypress', (Event) =>
             document.getElementById("com-score").innerHTML = "Win"; document.getElementById("Player-Score").innerHTML = "Lose";
         }
     }
+    
     else if(keycode == 3){
         player.innerHTML = ' <img src="media/3.jpg" style="width: 80px; transform: rotate(-60deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
         document.getElementById("com-score").innerHTML = "Lose"; document.getElementById("Player-Score").innerHTML = "Win";
@@ -46,17 +48,17 @@ document.addEventListener('keypress', (Event) =>
         null;
     }
 }, false);
+
 document.onkeydown = function (r){
     if (r.ctrlKey && (r.keyCode === 67 || r.keyCode === 86 || r.keyCode === 85 || r.keyCode === 117))
         { alert("Mau Ngapain ??"); return false;}
 };
 
-function BStart(Event)
-{
+function BStart(Event){
     location.reload();
 };
-function BHelp(Event)
-{
+
+function BHelp(Event){
     let text1 = "Tekan 1 untuk gunting"; let text2 = "Tekan 2 untuk Batu"; const text3 = "Tekan 3 untuk Kertas"; let text4 = "Tekan S Untuk Mulai";
     alert(text1 + "\n" + text2 + "\n" +text3 + "\n" +text4);
 };
@@ -71,3 +73,57 @@ document.addEventListener('keypress', (Event) =>{
         alert(text1 + "\n" + text2 + "\n" +text3 + "\n" +text4);
     };
 });
+
+function Btn_1(){
+    var digit = Math.floor(Math.random(digit) * 3);
+    let player = document.getElementById('out-player');
+    let bot = document.getElementById('out-com');
+
+    player.innerHTML = ' <img src="media/1.jpg" style="width: 80px; transform: rotate(-120deg); position: absolute; margin-top: 35px; margin-left: -40px" />';
+        if (digit == 0){
+            bot.innerHTML = ' <img src="media/1.jpg" style="width: 80px; transform: rotate(80deg); position: absolute; margin-top: 35px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Draw"; document.getElementById("Player-Score").innerHTML = "Draw";
+        }else if(digit == 1){
+            bot.innerHTML = ' <img src="media/2.jpg" style="width: 80px; transform: rotate(-90deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Win"; document.getElementById("Player-Score").innerHTML = "Lose";
+        }else{
+            bot.innerHTML = ' <img src="media/3.jpg" style="width: 80px; transform: rotate(120deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Lose"; document.getElementById("Player-Score").innerHTML = "Win";
+        }
+};
+
+function Btn_2(){
+    var digit = Math.floor(Math.random(digit) * 3);
+    let player = document.getElementById('out-player');
+    let bot = document.getElementById('out-com');
+
+    player.innerHTML = ' <img src="media/2.jpg" style="width: 80px; transform: rotate(90deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+        if (digit == 0){
+            bot.innerHTML = ' <img src="media/1.jpg" style="width: 80px; transform: rotate(80deg); position: absolute; margin-top: 35px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Lose"; document.getElementById("Player-Score").innerHTML = "Win";
+        }else if(digit == 1){
+            bot.innerHTML = ' <img src="media/2.jpg" style="width: 80px; transform: rotate(-90deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Draw"; document.getElementById("Player-Score").innerHTML = "Draw";
+        }else{
+            bot.innerHTML = ' <img src="media/3.jpg" style="width: 80px; transform: rotate(120deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Win"; document.getElementById("Player-Score").innerHTML = "Lose";
+        }
+};
+
+function Btn_3(){
+    var digit = Math.floor(Math.random(digit) * 3);
+    let player = document.getElementById('out-player');
+    let bot = document.getElementById('out-com');
+
+    player.innerHTML = ' <img src="media/3.jpg" style="width: 80px; transform: rotate(-60deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+        document.getElementById("com-score").innerHTML = "Lose"; document.getElementById("Player-Score").innerHTML = "Win";
+        if (digit == 0){
+            bot.innerHTML = ' <img src="media/1.jpg" style="width: 80px; transform: rotate(80deg); position: absolute; margin-top: 35px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Win"; document.getElementById("Player-Score").innerHTML = "Lose";
+        }else if(digit == 1){
+            bot.innerHTML = ' <img src="media/2.jpg" style="width: 80px; transform: rotate(-90deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+        }else{
+            bot.innerHTML = ' <img src="media/3.jpg" style="width: 80px; transform: rotate(120deg); position: absolute; margin-top: 25px; margin-left: -40px" />';
+            document.getElementById("com-score").innerHTML = "Draw"; document.getElementById("Player-Score").innerHTML = "Draw";
+        }
+};
